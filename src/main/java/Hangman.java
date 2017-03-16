45cc53ccbd85e79a50a7c7c7f8f3daed7d873ce1
Cameron Jensen and Mark Fisher <mark@>
2017-03-16 11:14:23 -0700

    Add get random word from words array

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -5,11 +5,16 @@ import java.util.Random;
 public class Hangman{
   private String[] words = {"buffalo", "luxury", "oxygen", "rhythm", "scratch", "voodoo", "wizard", "zigzag", "jinx", "frizzled"};
   private Integer randomInt = new Random().nextInt(words.length);
+
   public Hangman(){
 
   }
 
   public Integer getRandomInt(){
     return randomInt;
+  }
+
+  public String randomWord() {
+    return words[randomInt];
   }
 }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -10,9 +10,23 @@ public class HangmanTest {
   }
 
   @Test
-  public void Hangman_pickWordAtRandom_true() {
+  public void Hangman_getsRandomInt_true(){
+    Hangman testHangman = new Hangman();
+    assertEquals(true, testHangman.getRandomInt() instanceof Integer);
+  }
+
+  @Test
+  public void Hangman_pickRandomInteger_true() {
     Hangman testHangman = new Hangman();
     Integer wordIndex = testHangman.getRandomInt();
     assertEquals(true, wordIndex instanceof Integer);
    }
+
+   @Test
+   public void Hangman_pickRandomWord_true() {
+     Hangman testHangman = new Hangman();
+     String word = testHangman.randomWord();
+     assertEquals(true, word instanceof String);
+   }
+
 }
